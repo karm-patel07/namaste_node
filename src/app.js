14c5eb0cff1b1d3ddd06a5d91988f269e8f5f0a0
kk patel <patelkarm1007@gmail.com>
2025-12-20 12:53:18 +0530

    "api add in the code"

--- src/app.js
+++ src/app.js
@@ -41,24 +41,95 @@
 //   console.log(`Example app listening on port ${port}`);
 // });
 
-const adminAuth = require("./middleware/auth");
+//const adminAuth = require("./middleware/auth");
 const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/User");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
-// Admin authentication middleware
-app.use("/admin", adminAuth);
+// // Admin authentication middleware
+// app.use("/admin", adminAuth);
 
-// Admin success route
-app.get("/admin/success", (req, res) => {
-  res.send("Get API is called successfully");
-});
+// // Admin success route
+// app.get("/admin/success", (req, res) => {
+//   res.send("Get API is called successfully");
+// });
 
-app.get("/user", (req, res) => {
-  res.send("Get API is called successfully To The  User");
-});
+// app.get("/user", (req, res) => {
+//   res.send("Get API is called successfully To The  User");
+// });
 
 // Server start
-app.listen(port, () => {
-  console.log(`Your server started on port ${port}`);
-});
+
+connectDB()
+  .then(() => {
+    console.log("database connection successfully ");
+
+    app.post("/signup", async (req, res) => {
+      const user = new User([
+        {
+          firstName: "kk",
+          lastName: "patel",
+          emailId: "abc@test.com",
+          password: "abc123",
+          age: 23,
+          gender: "Male",
+        },
+        {
+          firstName: "Rahul",
+          lastName: "Sharma",
+          emailId: "rahul@test.com",
+          password: "rahul123",
+          age: 24,
+          gender: "Male",
+        },
+        {
+          firstName: "Priya",
+          lastName: "Mehta",
+          emailId: "priya@test.com",
+          password: "priya123",
+          age: 22,
+          gender: "Female",
+        },
+        {
+          firstName: "Aman",
+          lastName: "Patel",
+          emailId: "aman@test.com",
+          password: "aman123",
+          age: 27,
+          gender: "Male",
+        },
+        {
+          firstName: "Neha",
+          lastName: "Verma",
+          emailId: "neha@test.com",
+          password: "neha123",
+          age: 25,
+          gender: "Female",
+        },
+      ]);
+      try {
+        await user.save();
+        res.send("user data add successfully");
+      } catch (err) {
+        res.status(400).send("some error occur ", +err.massage);
+      }
+    });
+    app.get("/users", async (req, res) => {
+      try {
+        const users = await User.find(); // fetch all users
+        res.json(users);
+      } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
+      }
+    });
+
+    app.listen(port, () => {
+      console.log(`Your server started on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(" not connect to database ");
+  });
