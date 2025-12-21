2b623afeaf83dea91217886b21af57138a386f95
kk patel <patelkarm1007@gmail.com>
2025-12-21 15:36:03 +0530

    delete-get-post api added

--- src/app.js
+++ src/app.js
@@ -43,10 +43,12 @@
 
 //const adminAuth = require("./middleware/auth");
 const express = require("express");
+
 const connectDB = require("./config/database");
 const User = require("./models/User");
 const mongoose = require("mongoose");
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 // // Admin authentication middleware
@@ -68,61 +70,50 @@ connectDB()
     console.log("database connection successfully ");
 
     app.post("/signup", async (req, res) => {
-      const user = new User([
-        {
-          firstName: "kk",
-          lastName: "patel",
-          emailId: "abc@test.com",
-          password: "abc123",
-          age: 23,
-          gender: "Male",
-        },
-        {
-          firstName: "Rahul",
-          lastName: "Sharma",
-          emailId: "rahul@test.com",
-          password: "rahul123",
-          age: 24,
-          gender: "Male",
-        },
-        {
-          firstName: "Priya",
-          lastName: "Mehta",
-          emailId: "priya@test.com",
-          password: "priya123",
-          age: 22,
-          gender: "Female",
-        },
-        {
-          firstName: "Aman",
-          lastName: "Patel",
-          emailId: "aman@test.com",
-          password: "aman123",
-          age: 27,
-          gender: "Male",
-        },
-        {
-          firstName: "Neha",
-          lastName: "Verma",
-          emailId: "neha@test.com",
-          password: "neha123",
-          age: 25,
-          gender: "Female",
-        },
-      ]);
       try {
-        await user.save();
-        res.send("user data add successfully");
+        const user = new User(req.body); // 👈 data from Postman
+        const savedUser = await user.save();
+
+        res.status(201).json({
+          message: "User created successfully",
+          user: savedUser,
+        });
       } catch (err) {
-        res.status(400).send("some error occur ", +err.massage);
+        res.status(400).json({
+          error: err.message,
+        });
       }
     });
     app.get("/users", async (req, res) => {
+      const emailId = req.body.emailId;
       try {
-        const users = await User.find(); // fetch all users
-        res.json(users);
+        const users = await User.find({ emailId: emailId }); // fetch all users
+        res.send(users);
       } catch (error) {
         res.status(500).json({ message: "Something went wrong" });
+      }
+    });
+
+    app.delete("/users", async (req, res) => {
+      const { id } = req.body;
+
+      try {
+        const user = await User.findByIdAndDelete(id);
+
+        if (!user) {
+          return res.status(404).json({
+            message: "User not found",
+          });
+        }
+
+        res.json({
+          message: "User deleted successfully",
+          deletedUser: user,
+        });
+      } catch (error) {
+        res.status(400).json({
+          error: error.message,
+        });
       }
     });
 
